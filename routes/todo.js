94308328cf6cc28d5593eab584b978accf2a341f
Fawale Timmy <fawaletimmy1@gmail..com>
2020-04-28 16:53:46 +0100

    few changes

--- routes/todo.js
+++ routes/todo.js
@@ -63,9 +63,11 @@ router.post('/',async (req,res) => {
 router.delete('/', async (req,res) => {
 // The "todo" in this callback function represents the document that was found.
 // It allows you to pass a reference back to the client in case they need a reference for some reason.
-Todo.findOneAndRemove(req.params.title, (err, todo) => {
+Todo.findOneAndRemove(req.params.id, (err, todo) => {
     // As always, handle any potential errors:
-    if (err) return res.status(500).send(err);
+    if (err) {
+        return res.status(500).send(err);
+    }
     // We'll create a simple object to send back with a message and the id of the document that was removed
     // You can really do this however you want, though.
     const response = {
